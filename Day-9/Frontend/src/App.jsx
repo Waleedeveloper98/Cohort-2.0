bd8460354056510074fbc7ba04471b2183738c2e
Waleed <waleedeveloper100@gmail.com>
2026-02-03 22:15:46 +0500

    frontend apis changed

--- Day-9/Frontend/src/App.jsx
+++ Day-9/Frontend/src/App.jsx
@@ -5,9 +5,10 @@ const App = () => {
   const [notesData, setNotesData] = useState([]);
 
   const fetchAllNotes = () => {
-    axios.get("http://localhost:3000/api/notes").then((res) => {
+    axios.get("http://localhost:3000/api/v1/notes").then((res) => {
       setNotesData(res.data.notes);
       console.log(res.data.notes);
+      console.log("dlkajd")
     });
   };
 
@@ -16,7 +17,7 @@ const App = () => {
     const { title, description } = e.target.elements;
     console.log(title.value, description.value);
     axios
-      .post("http://localhost:3000/api/notes", {
+      .post("http://localhost:3000/api/v1/notes", {
         title: title.value,
         description: description.value,
       })
@@ -27,7 +28,7 @@ const App = () => {
   };
 
   const handleDeleteNote = (noteId) => {
-    axios.delete(`http://localhost:3000/api/notes/${noteId}`).then((res) => {
+    axios.delete(`http://localhost:3000/api/v1/notes/${noteId}`).then((res) => {
       console.log(res.data);
       fetchAllNotes();
     });
@@ -36,6 +37,10 @@ const App = () => {
   useEffect(() => {
     fetchAllNotes();
   }, []);
+
+  useEffect(()=>{
+    console.log(notesData)
+  },[notesData])
 
   return (
     <div className="app">
