989824bd69a11dd2fd27f27047fd69e16e07529a
Waleed <waleedeveloper100@gmail.com>
2026-02-11 10:27:16 +0500

    userSchema and userModel created

---
+++ insta-clone/src/models/user.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: [true, "Username already exist."],
+        required: [true, "Username is required."]
+    },
+    email: {
+        type: String,
+        unique: [true, "Email already exist."],
+        required: [true, "Email is required."]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required."]
+    },
+    bio: {
+        type: String
+    },
+    profileImage: {
+        type: String,
+        default: "https://ik.imagekit.io/todbuwbsb/user%20profile"
+    }
+})
+
+const userModel = mongoose.model("users", userSchema)
+
+module.exports = userModel
