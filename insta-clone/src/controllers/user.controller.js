db419db32a02cdb0d016afa261e8355a47cbe571
Waleed <waleedeveloper100@gmail.com>
2026-02-18 14:58:38 +0500

    feat: implement follow user functionality with validation

---
+++ insta-clone/src/controllers/user.controller.js
@@ -0,0 +1,48 @@
+const followModel = require("../models/follow.model");
+const userModel = require("../models/user.model");
+
+const followUserController = async (req, res) => {
+    const followerUsername = req.user.username;
+    const followeeUsername = req.params.username;
+
+    if (followeeUsername === followeeUsername) {
+        return res.status(400).json({
+            message: "You cannot follow yourself"
+        })
+    }
+
+    const isFolloweeExist = await userModel.findOne({
+        username: followeeUsername
+    })
+
+    if (!isFolloweeExist) {
+        return res.status(404).json({
+            message: "Wrong username"
+        })
+    }
+
+    const isAlreadyFollowing = await followModel.findOne({
+        follower: followerUsername,
+        followee: followeeUsername
+    })
+
+    if (isAlreadyFollowing) {
+        return res.status(400).json({
+            message: "You already following this user"
+        })
+    }
+
+    const follow = await followModel.create({
+        follower: followerUsername,
+        followee: followeeUsername
+    })
+
+    res.status(201).json({
+        message: "successfully followed user",
+        follow
+    })
+}
+
+module.exports = {
+    followUserController
+}
---
+++ insta-clone/src/routes/user.routes.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const identifyUser = require("../middlewares/auth.middleware")
+const followRouter = express.Router()
+const followController = require("../controllers/user.controller")
+
+followRouter.post("/follow/:username", identifyUser, followController.followUserController)
+
+module.exports = followRouter
